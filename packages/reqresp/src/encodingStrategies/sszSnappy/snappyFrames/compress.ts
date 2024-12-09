7369c2563f0ed62a15e2c90ea881c7225e60041f
Cayman <caymannava@gmail.com>
2024-12-09 15:59:23 -0500

    chore: convert Uint8Array to Buffer

--- packages/reqresp/src/encodingStrategies/sszSnappy/snappyFrames/compress.ts
+++ packages/reqresp/src/encodingStrategies/sszSnappy/snappyFrames/compress.ts
@@ -32,7 +32,9 @@ export async function* encodeSnappy(bytes: Buffer): AsyncGenerator<Buffer> {
   yield IDENTIFIER_FRAME;
 
   for (let i = 0; i < bytes.length; i += UNCOMPRESSED_CHUNK_SIZE) {
-    const chunk = bytes.subarray(i, i + UNCOMPRESSED_CHUNK_SIZE);
+    const _chunk = bytes.subarray(i, i + UNCOMPRESSED_CHUNK_SIZE);
+    // in Bun, _chunk is a Uint8Array, needs to be a Buffer
+    const chunk = Buffer.from(_chunk.buffer, _chunk.byteOffset, _chunk.byteLength);
     const compressed = snappy.compressSync(chunk);
     if (compressed.length < chunk.length) {
       const size = compressed.length + 4;
